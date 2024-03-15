1464b8438e98a14c2e548861a6a4c9e45fb73aa1
violettomania <danielpolya@protonmail.com>
2024-03-15 13:03:23 +0100

    move projects up to the first place in Navbar

--- src/lib/data/navbar.ts
+++ src/lib/data/navbar.ts
@@ -1,6 +1,6 @@
 export const items = [
-	{ title: 'Skills', to: '/skills', icon: 'i-carbon-software-resource-cluster' },
 	{ title: 'Projects', to: '/projects', icon: 'i-carbon-cube' },
+	{ title: 'Skills', to: '/skills', icon: 'i-carbon-software-resource-cluster' },
 	{ title: 'Experience', to: '/experience', icon: 'i-carbon-development' },
 	{ title: 'Education', to: '/education', icon: 'carbon--education' },
 	{ title: 'Resumé', to: '/resume', icon: 'carbon--result' }
