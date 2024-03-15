f38b0e7af653def333390619418faf1f00e124ce
violettomania <danielpolya@protonmail.com>
2024-03-15 13:01:42 +0100

    add resume and education icons

--- src/lib/data/navbar.ts
+++ src/lib/data/navbar.ts
@@ -2,6 +2,6 @@ export const items = [
 	{ title: 'Skills', to: '/skills', icon: 'i-carbon-software-resource-cluster' },
 	{ title: 'Projects', to: '/projects', icon: 'i-carbon-cube' },
 	{ title: 'Experience', to: '/experience', icon: 'i-carbon-development' },
-	{ title: 'Education', to: '/education', icon: 'i-carbon:education' },
-	{ title: 'Resumé', to: '/resume', icon: 'i-carbon-result' }
+	{ title: 'Education', to: '/education', icon: 'carbon--education' },
+	{ title: 'Resumé', to: '/resume', icon: 'carbon--result' }
 ] as const;
