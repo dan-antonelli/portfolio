8f6c89748736ed267182c1055037a50f4d684517
violettomania <danielpolya@protonmail.com>
2024-03-15 13:16:17 +0100

    modify the title a bit

--- src/lib/data/home.ts
+++ src/lib/data/home.ts
@@ -7,7 +7,7 @@ export const name = 'Daniel';
 
 export const lastName = 'Polya';
 
-export const mainTitle = "Hi! I'm Daniel";
+export const mainTitle = "Hi! I'm Daniel,";
 
 export const description =
 	'a seasoned software developer specializing in building dynamic web applications. I have a strong background in technologies ranging from Java to JavaScript, with a current focus on React. Check out my projects and blog posts!';
