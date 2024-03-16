9b2f9eb9925c8bf1bf69fb4a0b357de746c4fb43
violettomania <danielpolya@protonmail.com>
2024-03-16 15:08:20 +0100

    amend study experiences

--- src/lib/data/education.ts
+++ src/lib/data/education.ts
@@ -11,8 +11,8 @@ export const items: Array<Education> = [
 		name: '',
 		organization: 'Braining Hub - Education Center',
 		period: { from: new Date(2018, 0, 1), to: new Date(2018, 7, 1) },
-		shortDescription: '',
-		slug: 'dummy-education-item-2',
+		shortDescription: 'Java developer bootcamp.',
+		slug: 'java-developer-bootcamp',
 		subjects: ['Algorithms and Data structures', 'Java', 'Spring', 'Relational databases', 'Agile']
 	},
 	{
@@ -21,11 +21,11 @@ export const items: Array<Education> = [
 			'A 4-month course organized by Udacity and Google. Studied Java and UI development for Android apps.',
 		location: 'Online',
 		logo: Assets.Android,
-		name: '',
+		name: 'Android scholarship',
 		organization: 'Google',
 		period: { from: new Date(2017, 0, 1), to: new Date(2018, 0, 1) },
-		shortDescription: '',
-		slug: 'dummy-education-item-2',
+		shortDescription: 'Android scholarship by Google and Udacity.',
+		slug: 'android-scholarship',
 		subjects: ['Java', 'Android', 'UI development']
 	},
 	{
@@ -33,10 +33,10 @@ export const items: Array<Education> = [
 		description: 'Hungarian Language and Literature',
 		location: 'Debrecen, Hungary',
 		logo: Assets.University,
-		name: '',
+		name: 'Bachelor of Arts',
 		organization: 'University of Debrecen',
 		period: { from: new Date(2006, 0, 1), to: new Date(2010, 0, 1) },
-		shortDescription: 'dwwefe',
+		shortDescription: 'Bachelor of Arts',
 		slug: 'faculty-of-humanities',
 		subjects: ['Grammar', 'Literature', 'Pedagogy']
 	}
