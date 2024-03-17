4d5945f4cfed8bde91ab72c7f3a56fd65f110bd4
violettomania <danielpolya@protonmail.com>
2024-03-17 13:01:20 +0100

    remove odd jobs

--- src/lib/data/experience.ts
+++ src/lib/data/experience.ts
@@ -12,7 +12,7 @@ export const items: Array<Experience> = [
 		location: 'Budapest, Hungary',
 		period: { from: new Date(2022, 2, 1), to: new Date() },
 		skills: getSkills('ts', 'js', 'reactjs'),
-		name: 'Full-stack developer',
+		name: 'Senior Frontend Developer',
 		color: 'red',
 		links: [],
 		logo: Assets.ReactJs,
@@ -39,7 +39,7 @@ export const items: Array<Experience> = [
 		slug: 'full-stack-developer',
 		company: 'DSS Consulting Kft.',
 		description:
-			'Full-stack enterprise software development on international projects using Java 8 (Spring/Java EE) and TypeScript (Angular).',
+			'Full-stack enterprise software development on international projects using Java 8 (Spring/Java EE) and React/Typescript.',
 		contract: ContractType.FullTime,
 		type: 'Software Development',
 		location: 'Budapest, Hungary',
@@ -68,28 +68,11 @@ export const items: Array<Experience> = [
 		logo: Assets.Writing,
 		shortDescription:
 			'Producing/editing documentation and technical illustrations for telecommunication software projects. Keeping contact with developer teams in Finland and Sweden. Automating tasks using Python.'
-	},
-	{
-		slug: 'english-teacher',
-		company: 'Alma Mater Language School',
-		description: 'Holding English courses for adults.',
-		contract: ContractType.FullTime,
-		type: 'Education',
-		location: 'Budapest, Hungary',
-		period: { from: new Date(2014, 0, 1), to: new Date(2016, 12, 1) },
-		skills: getSkills('english'),
-		name: 'English Teacher',
-		color: 'green',
-		links: [],
-		logo: Assets.University,
-		shortDescription: 'Holding English courses for adults.'
 	}
 ];
 
 export const title = 'Experience';
 
-// TODO: no color on cards
-// TODO: no animation on cards
 // TODO: java icon too small
 // TODO: add banking jobs
 // TODO: more detailed descriptions
