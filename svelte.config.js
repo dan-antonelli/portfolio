2b1b5e6b78aa83acc84af1ca5d564049d7b3b54f
violettomania <danielpolya@protonmail.com>
2024-03-21 19:34:44 +0100

    tryng to fix build error

--- svelte.config.js
+++ svelte.config.js
@@ -26,9 +26,6 @@ const config = {
 		},
 		paths: {
 			base: process.env.NODE_ENV === 'production' ? base : ''
-		},
-		prerender: {
-			pages: ['*', '/experience/*', '/projects/*', '/skills/*']
 		}
 	}
 };
