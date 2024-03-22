dda1edef35a8f0fe7d9739f36cce21a969ccf595
violettomania <danielpolya@protonmail.com>
2024-03-22 15:16:49 +0100

    add bigger projects

--- src/lib/data/projects.ts
+++ src/lib/data/projects.ts
@@ -4,12 +4,17 @@ import type { Project } from '../types';
 
 import jobs from '@images/projects/jobs.png';
 import webshop from '@images/projects/webshop.png';
+import menu from '@images/projects/menu.png';
+import cocktails from '@images/projects/cocktails.png';
+import colorGenerator from '@images/projects/color-generator.png';
+import cart from '@images/projects/cart.png';
+import groceryList from '@images/projects/grocery-list.png';
 
 export const items: Array<Project> = [
 	{
 		slug: 'webshop',
 		color: '#5e95e3',
-		description: 'Home design webshop.',
+		description: 'Home design webshop',
 		shortDescription: 'Home design webshop',
 		links: [{ to: 'https://github.com/violettomania/webshop', label: 'GitHub' }],
 		logo: Assets.ReactJs,
@@ -22,7 +27,7 @@ export const items: Array<Project> = [
 	{
 		slug: 'jobs-tracker',
 		color: '#5e95e3',
-		description: 'Job tracking app to help you keep track of your job applications.',
+		description: 'Job tracking app to help you keep track of your job applications',
 		shortDescription: 'Job tracking app to help you keep track of your job applications',
 		links: [{ to: 'https://github.com/violettomania/jobs', label: 'GitHub' }],
 		logo: Assets.ReactJs,
@@ -31,6 +36,71 @@ export const items: Array<Project> = [
 		type: 'Frontend App',
 		href: 'https://violettomania.github.io/jobs',
 		img: jobs
+	},
+	{
+		slug: 'menu',
+		color: '#5e95e3',
+		description: 'Searchable restaurant menu',
+		shortDescription: 'Searchable restaurant menu',
+		links: [{ to: 'https://github.com/violettomania/menu', label: 'GitHub' }],
+		logo: Assets.ReactJs,
+		name: 'Searchable restaurant menu',
+		skills: getSkills('reactjs', 'redux', 'ts'),
+		type: 'Frontend App',
+		href: 'https://violettomania.github.io/menu',
+		img: menu
+	},
+	{
+		slug: 'cocktails',
+		color: '#5e95e3',
+		description: 'Cocktail recipes',
+		shortDescription: 'Cocktail recipes',
+		links: [{ to: 'https://github.com/violettomania/cocktails', label: 'GitHub' }],
+		logo: Assets.ReactJs,
+		name: 'Cocktail recipes',
+		skills: getSkills('reactjs', 'redux', 'ts'),
+		type: 'Frontend App',
+		href: 'https://violettomania.github.io/cocktails',
+		img: cocktails
+	},
+	{
+		slug: 'color-generator',
+		color: '#5e95e3',
+		description: 'Hex color generator',
+		shortDescription: 'Hex color generator',
+		links: [{ to: 'https://github.com/violettomania/color-generator', label: 'GitHub' }],
+		logo: Assets.ReactJs,
+		name: 'Hex color generator',
+		skills: getSkills('reactjs', 'redux', 'ts'),
+		type: 'Frontend App',
+		href: 'https://violettomania.github.io/color-generator',
+		img: colorGenerator
+	},
+	{
+		slug: 'cart',
+		color: '#5e95e3',
+		description: 'Simple cart',
+		shortDescription: 'Simple cart',
+		links: [{ to: 'https://github.com/violettomania/cart', label: 'GitHub' }],
+		logo: Assets.ReactJs,
+		name: 'Simple cart',
+		skills: getSkills('reactjs', 'redux', 'ts'),
+		type: 'Frontend App',
+		href: 'https://violettomania.github.io/cart',
+		img: cart
+	},
+	{
+		slug: 'grocery-list',
+		color: '#5e95e3',
+		description: 'Grocery list',
+		shortDescription: 'Grocery list',
+		links: [{ to: 'https://github.com/violettomania/grocery-bud', label: 'GitHub' }],
+		logo: Assets.ReactJs,
+		name: 'Grocery list',
+		skills: getSkills('reactjs', 'redux', 'ts'),
+		type: 'Frontend App',
+		href: 'https://violettomania.github.io/grocery-bud',
+		img: groceryList
 	}
 ];
 
