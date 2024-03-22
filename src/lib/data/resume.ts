4e9187b5cd212f7e3c256c896d028824402e0f99
violettomania <danielpolya@protonmail.com>
2024-03-22 19:01:54 +0100

    update resume link

--- src/lib/data/resume.ts
+++ src/lib/data/resume.ts
@@ -1,4 +1,4 @@
 export const data =
-	'https://drive.google.com/file/d/1xoabIDeWScO41TYs7MY_F4_JnOyijF-U/view?usp=drive_link';
+	'https://drive.google.com/file/d/1haZEKyq05V6wDI6MJxvaMqBjxcgVNyIZ/view?usp=drive_link';
 
 export const title = 'Resumé';
