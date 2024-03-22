ff442ddec8f339ed68c20e562901e1fe4d1e0a43
violettomania <147735091+violettomania@users.noreply.github.com>
2024-03-22 19:06:26 +0000

    Deploying to gh-pages from @ violettomania/portfolio@3863519b2ba66d4307cc0cac265f16c74f2d063e 🚀

---
+++ _app/immutable/chunks/posts.a6dc34e0.js
@@ -0,0 +1,1 @@
+import{A as t}from"./index.2360f73f.js";import{g as o}from"./skills.3de204e4.js";const i=[{slug:"lorem-ipsum",color:"#5e95e3",description:"Dummy blog post",shortDescription:"Just testing the blog post layout. Come back later for real content! :)",links:[],logo:t.Writing,name:"Blog post title",period:{from:new Date},skills:o("reactjs"),type:""}],l="Blog posts";export{i,l as t};
