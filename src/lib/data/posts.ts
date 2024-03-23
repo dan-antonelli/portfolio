9ef4bbb73b30d6453d0daacb3f8cd62e5e12b4cf
violettomania <danielpolya@protonmail.com>
2024-03-23 18:07:40 +0100

    testing markdown on dummy blog post

--- src/lib/data/posts.ts
+++ src/lib/data/posts.ts
@@ -6,7 +6,10 @@ export const items: Array<Post> = [
 	{
 		slug: 'test-post',
 		color: '#5e95e3',
-		description: 'Dummy blog post',
+		description: `What is Lorem Ipsum?
+--------------------
+
+**Lorem Ipsum** is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.`,
 		shortDescription: 'Just testing the blog post layout. Come back later for real content! :)',
 		links: [],
 		logo: Assets.Writing,
