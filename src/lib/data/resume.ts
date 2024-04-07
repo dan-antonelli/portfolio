e58f45dcb5b151b94a26751cbdff5ad7c8b45b37
violettomania <danielpolya@protonmail.com>
2024-04-07 22:29:18 +0200

    update resume lin

--- src/lib/data/resume.ts
+++ src/lib/data/resume.ts
@@ -1,4 +1,4 @@
 export const data =
-	'https://drive.google.com/file/d/1KrUeTffpRDm8DJqhJcOzGrIr6RfGe9Rr/view?usp=drive_link';
+	'https://drive.google.com/file/d/13l7aETsGICbz_AAhcGPb09US_cl_rJk_/view?usp=drive_link';
 
 export const title = 'Resumé';
