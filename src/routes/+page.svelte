43b2cf7755d68d299205bede5d6fd403e3459469
violettomania <danielpolya@protonmail.com>
2024-04-25 13:51:35 +0200

    WIP one-page layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,6 +9,7 @@
 	import { isBlank } from '@riadh-adrani/utils';
 	import { getPlatfromIcon } from '$lib/utils';
 	import avatar from '@images/avatar.png';
+	import BlogPage from '../routes/blog/+page.svelte';
 
 	const isEmail = (email: string): boolean => {
 		const reg =
@@ -64,3 +65,4 @@
 		</div>
 	</div>
 </div>
+<BlogPage />
