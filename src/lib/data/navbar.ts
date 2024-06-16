8997ca4cd4e4f268f3abedf56b8998686f5896ea
violettomania <danielpolya@protonmail.com>
2024-06-16 16:31:19 +0200

    remove blog and stories from navbar

--- src/lib/data/navbar.ts
+++ src/lib/data/navbar.ts
@@ -1,7 +1,7 @@
 export const items = [
+	// { title: 'Blog', to: '/blog', icon: 'i-carbon-blog' },
+	// { title: 'Stories', to: '/short-stories', icon: 'i-carbon-book' },
 	{ title: 'Projects', to: '/projects', icon: 'i-carbon-cube' },
-	{ title: 'Blog', to: '/blog', icon: 'i-carbon-blog' },
-	{ title: 'Stories', to: '/short-stories', icon: 'i-carbon-book' },
 	{ title: 'Skills', to: '/skills', icon: 'i-carbon-software-resource-cluster' },
 	{ title: 'Experience', to: '/experience', icon: 'i-carbon-development' },
 	{ title: 'Education', to: '/education', icon: 'i-carbon-education' },
